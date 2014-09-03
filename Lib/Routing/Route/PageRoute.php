aca7e1ff0252f0d62522ce59c1b36245c509a69d
Jose Diaz-Gonzalez <josegonzalez@users.noreply.github.com>
2014-09-03 10:27:07 -0400

    Fix docblock

--- Lib/Routing/Route/PageRoute.php
+++ Lib/Routing/Route/PageRoute.php
@@ -46,7 +46,7 @@ class PageRoute extends CakeRoute {
  *
  * @param string $template Template string with parameter placeholders
  * @param array $defaults Array of defaults for the route.
- * @param string $params Array of parameters and additional options for the Route
+ * @param string $options Array of parameters and additional options for the Route
  * @return void
  */
 	public function __construct($template, $defaults = array(), $options = array()) {
